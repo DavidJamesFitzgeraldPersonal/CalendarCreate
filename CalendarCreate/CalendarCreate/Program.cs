b09f11d62b4896d6ab35898ac69ddb76dd90451d
David Fitzgerald <david.fitzgerald@tracerco.com>
2022-08-29 17:07:14 +0100

    Initial commit - Hello World

--- CalendarCreate/CalendarCreate/Program.cs
+++ CalendarCreate/CalendarCreate/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CalendarCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (2 != args.Length)
+            {
+                Console.WriteLine("Please specify the year of interest (in decimal notation from 1582) and the required file name \r\n" +
+                                    "seperated by a space. e.g. \r\n" +
+                                    "1582 fileName");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
