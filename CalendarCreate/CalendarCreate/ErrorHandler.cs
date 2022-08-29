b0836cc1d72cb93569cbd6a9c7aa5b1a0580e0da
David Fitzgerald <david.fitzgerald@tracerco.com>
2022-08-29 17:43:46 +0100

    User input verified and errors handled gracefully

---
+++ CalendarCreate/CalendarCreate/ErrorHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CalendarCreate
+{
+    static class ErrorHandler
+    {
+        public enum eERRORS
+        { 
+            USAGE = -1,
+            YEAR = -2,
+            FILE_LENGTH = -3,
+            FILE_NAME = -4
+        };
+
+        public static int HandleError(eERRORS type)
+        {
+            switch(type)
+            {
+                case eERRORS.USAGE:
+                    Console.WriteLine("USAGE ERROR: Please specify the year of interest (in decimal notation from 1582) and the required file name \r\n" +
+                                        "seperated by a space. e.g. \r\n" +
+                                        "1582 fileName");
+                    break;
+
+                case eERRORS.YEAR:
+                    Console.WriteLine("YEAR ERROR: Please specify the year of interest in decimal notation from 1582");
+                    break;
+
+                case eERRORS.FILE_LENGTH:
+                    Console.WriteLine("FILE NAME LENGTH ERROR: Please specify a file name of less than 100 characters");
+                    break;
+
+                case eERRORS.FILE_NAME:
+                    Console.WriteLine("FILE NAME ERROR: Please specify a valid file name");
+                    break;
+
+                default:
+                    Console.WriteLine("Unhandled Error!");
+                    break;
+            }
+
+            return (int)type;
+        }
+    }
+}
--- CalendarCreate/CalendarCreate/Program.cs
+++ CalendarCreate/CalendarCreate/Program.cs
@@ -1,21 +1,44 @@
-﻿using System;
-
-namespace CalendarCreate
+﻿namespace CalendarCreate
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            const int FILE_NAME_MAX_LENGTH = 100;
+            int status = 0;
+
             if (2 != args.Length)
             {
-                Console.WriteLine("Please specify the year of interest (in decimal notation from 1582) and the required file name \r\n" +
-                                    "seperated by a space. e.g. \r\n" +
-                                    "1582 fileName");
+                status = ErrorHandler.HandleError(ErrorHandler.eERRORS.USAGE);
             }
             else
             {
+                /* Ensure the year entered is in a usable range - Top End ?*/
+                int tempYear = 0;
+                if ((false == int.TryParse(args[0], out tempYear)) || (tempYear < 1582))
+                {
+                    status = ErrorHandler.HandleError(ErrorHandler.eERRORS.YEAR);
+                }
 
-            }
+                /* Ensure the length of the filename is sensible - This is an abitrary limit! */
+                if (args[1].Length > FILE_NAME_MAX_LENGTH)
+                {
+                    status = ErrorHandler.HandleError(ErrorHandler.eERRORS.FILE_LENGTH);
+                }
+
+                /* Ensure the file name entered is formatted correctly*/
+                char[] badFileChars = System.IO.Path.GetInvalidFileNameChars();
+                foreach (char c in badFileChars)
+                {
+                    if (args[1].Contains(c))
+                    {
+                        status = ErrorHandler.HandleError(ErrorHandler.eERRORS.FILE_NAME);
+                        break;
+                    }
+                }
+
+            } //if (2 != args.Length)
+            return status;
         }
     }
 }
